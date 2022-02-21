d5042a77a012906ed5cc4b2af0a3ca7cd3deaeea
sawl-an-dawnen <elijah.salandanan95@gmail.com>
2022-02-21 00:47:04 -0600

    mod to main

--- Assignment_01.cpp
+++ Assignment_01.cpp
@@ -1,22 +1,9 @@
 #include<iostream>
 
 using namespace std;
-/*
+
 int main(int argc, char** argv) {
-
     cout << "HELLO WORLD" << endl;
-
-   //cout << "This program has " << argc << " arguments:" << endl;
-   //for (int i = 0; i < argc; ++i) {
-     // cout << argv[i] << endl;
-  // }
-   return 0;
-}
-*/
-int main() {
-
-    cout << "HELLO WORLD" << endl;
-
    //cout << "This program has " << argc << " arguments:" << endl;
    //for (int i = 0; i < argc; ++i) {
      // cout << argv[i] << endl;
