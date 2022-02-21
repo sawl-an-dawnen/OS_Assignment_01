75b18e16d87196406e9fa1fafb56d87cbe7d29a3
sawl-an-dawnen <elijah.salandanan95@gmail.com>
2022-02-21 00:42:51 -0600

    testing modifications

--- Assignment_01.cpp
+++ Assignment_01.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+
 using namespace std;
+/*
 int main(int argc, char** argv) {
-   cout << "This program has " << argc << " arguments:" << endl;
-   for (int i = 0; i < argc; ++i) {
-      cout << argv[i] << endl;
-   }
+
+    cout << "HELLO WORLD" << endl;
+
+   //cout << "This program has " << argc << " arguments:" << endl;
+   //for (int i = 0; i < argc; ++i) {
+     // cout << argv[i] << endl;
+  // }
+   return 0;
+}
+*/
+int main() {
+
+    cout << "HELLO WORLD" << endl;
+
+   //cout << "This program has " << argc << " arguments:" << endl;
+   //for (int i = 0; i < argc; ++i) {
+     // cout << argv[i] << endl;
+  // }
    return 0;
 }
