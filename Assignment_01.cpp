055f52078ed7a2162f5c556b26e0a1bf799423b7
sawl-an-dawnen <elijah.salandanan95@gmail.com>
2022-02-21 00:08:44 -0600

    adding file

---
+++ Assignment_01.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char** argv) {
+   cout << "This program has " << argc << " arguments:" << endl;
+   for (int i = 0; i < argc; ++i) {
+      cout << argv[i] << endl;
+   }
+   return 0;
+}
